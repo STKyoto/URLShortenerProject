9d7b8a1230c15320c482a569c6771a0d62fb31da
WHITE <dima.itsko.ua@gmail.com>
2025-04-12 20:45:16 +0300

    feat: add Link entity -added Link class mapped to the "links"table -defined relationships with User entity -includes fields for originalUrl, shortUrl, creadedAt, expiresAt

--- src/main/java/com/example/demo/model/Link.java
+++ src/main/java/com/example/demo/model/Link.java
@@ -1,4 +1,92 @@
 package com.example.demo.model;
 
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "links")
 public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "original_url", nullable = false, length = 2048)
+    private String originalUrl;
+
+    @Column(name = "short_url", nullable = false, unique = true, length = 12)
+    private String shortUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "username", nullable = false)
+    private User user;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "expires_at")
+    private LocalDateTime expiresAt;
+
+
+
+    //constructors, getters and setters
+    public Link() {
+
+    }
+
+    public Link(String originalUrl, String shortUrl, User user, LocalDateTime expiresAt) {
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+        this.user = user;
+        this.expiresAt = expiresAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
 }
