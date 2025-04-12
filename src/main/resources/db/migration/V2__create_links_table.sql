ee780310c098acec9d937dab92ae6bf3b2328458
linniking2 <38794009+linniking2@users.noreply.github.com>
2025-04-12 15:46:47 +0300

    Змінив dev, prod.properties. Додав створення базу для юзера та посилання з відповідними перевірками

---
+++ src/main/resources/db/migration/V2__create_links_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS links (
+     id SERIAL PRIMARY KEY,
+     original_url TEXT NOT NULL CHECK (original_url ~* '^(http|https)://'),
+     short_url VARCHAR(12) NOT NULL UNIQUE CHECK (short_url ~ '^[a-zA-Z0-9]+$'),
+     user_id INTEGER NOT NULL REFERENCES users(id),
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     expires_at TIMESTAMP,
+     CHECK (char_length(original_url) < 2048)
+);
