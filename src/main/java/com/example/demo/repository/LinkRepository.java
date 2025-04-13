ce2ac3cc4eea5882cf2356e6a849df633bfe829b
WHITE <dima.itsko.ua@gmail.com>
2025-04-14 01:57:26 +0300

    refactor LinkRepositrory and LinkService -updated LinkRepository to extend JpaRepository<Link, Integer> - in LinkService:   1) added validateShortLink() method to ensure original URL is valid using java.net.URL   2)generateShortUrl() now creates a random alphanumeric string between 6 to 8 characters   3) implemented createShortLink() with short URL uniqueness check and user lookup   4)added recordClick() to increment click count

--- src/main/java/com/example/demo/repository/LinkRepository.java
+++ src/main/java/com/example/demo/repository/LinkRepository.java
@@ -2,12 +2,13 @@ package com.example.demo.repository;
 
 
 import com.example.demo.model.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface LinkRepository {
+public interface LinkRepository  extends JpaRepository<Link, Integer> {
     Optional<Link> findByShortUrl(String url);
     boolean existsByShortUrl(String url);
 
