6956dda8c7db0ce64b812b347d9118c8e6ce8176
vitaliykuzmak <vitalikkuzmak98@gmail.com>
2025-04-13 12:39:23 +0200

    Додав User, UserRepository та UserService

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,4 +1,21 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
 public class UserRepository {
+
+    private final Map<String, User> fakeDb = new HashMap<>();
+
+    public Optional<User> findByUsername(String username) {
+        return Optional.ofNullable(fakeDb.get(username));
+    }
+
+    public void save(User user) {
+        fakeDb.put(user.getUsername(), user);
+    }
 }
+
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,27 @@
 package com.example.demo.service;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
-public class UserService implements UserDetailsService {
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    // Ін'єкція конструктора
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public void registerUser(User user) {
+        // можна додати валідацію, логування і т.д.
+        userRepository.save(user);
+    }
 }
