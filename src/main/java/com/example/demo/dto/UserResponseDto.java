8f9a9f3db2ca8136068fa874294f734b42e25597
linniking2 <38794009+linniking2@users.noreply.github.com>
2025-04-13 14:09:21 +0300

    Додано сервіси dto

---
+++ src/main/java/com/example/demo/dto/UserResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class UserResponseDto {
+    @Setter
+    @Getter
+    private Long id;
+
+    @Setter
+    @Getter
+    private String username;
+
+
+}
