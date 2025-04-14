10cad4162eef65752a24ee922cbfc6a4bd00dd26
maria-syrtseva <syrcevamaria6@gmail.com>
2025-04-14 20:22:38 +0300

    Контролери аутентифікації та реєстрації (JWT) + dto, AuthenticationService

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
+
 public class AuthController {
     private final AuthenticationService authService;
 
--- src/main/java/com/example/demo/controller/RegController.java
+++ src/main/java/com/example/demo/controller/RegController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
+
 public class RegController {
 
     private final UserRepository userRepository;
