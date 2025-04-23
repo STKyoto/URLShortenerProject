7ae2b5ed0de6aab1b2484db019e169a6a40f2544
WHITE <dima.itsko.ua@gmail.com>
2025-04-23 22:32:50 +0300

    saving changes before switching branches

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -23,8 +23,6 @@ public class User {
     @Column(name = "password_hash", nullable = false)
     private String password;
 
-    @Column(unique = true)
-    private String email;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Link> links = new ArrayList<>();
