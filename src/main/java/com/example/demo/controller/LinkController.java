21a283286516628e3f7337d9560b0841be6dad6d
Valentyn <mazurv916@gmail.com>
2025-04-25 18:24:50 +0200

    corrections

--- src/main/java/com/example/demo/controller/LinkController.java
+++ src/main/java/com/example/demo/controller/LinkController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -28,22 +27,18 @@ public class LinkController {
     @PostMapping("/create")
     public ResponseEntity<?> createLink(@RequestBody LinkRequestDto request,
                                         @AuthenticationPrincipal String username) {
-
         if (request.getOriginalUrl() == null || request.getOriginalUrl().isEmpty()) {
             return ResponseEntity.badRequest().body("Original URL must be provided.");
         }
-
         if (request.getExpiresAt() == null || request.getExpiresAt().isEmpty()) {
             return ResponseEntity.badRequest().body("Expiration date must be provided.");
         }
-
         LocalDateTime expiration;
         try {
             expiration = LocalDateTime.parse(request.getExpiresAt());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Expiration date format is invalid.");
         }
-
         Link createdLink = linkService.createShortLink(request.getOriginalUrl(), username, expiration);
         String shortUrlWithBase = baseUrl + "/" + createdLink.getShortUrl();
         LinkDto linkDto = LinkMapper.toDto(createdLink);
@@ -59,21 +54,16 @@ public class LinkController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("Link not found");
         }
-
         Link link = linkOpt.get();
-
         if (link.getExpiresAt() != null && link.getExpiresAt().isBefore(LocalDateTime.now())) {
             return ResponseEntity.status(HttpStatus.GONE)
                     .body("This link has expired");
         }
-
         linkService.recordClick(shortUrl);
-
         return ResponseEntity.status(HttpStatus.FOUND)
                 .location(URI.create(link.getOriginalUrl()))
                 .build();
     }
-
 
     @GetMapping("/{shortUrl}/stats")
     public long getClickStats(@PathVariable String shortUrl) {
